bb11b40e42872cc9dce147120868216aef54c1b8
Salvador Fernandez <salvador.fernandezm@gmail.com>
2026-04-24 20:50:55 -0600

    Aumentando limite de subida a 20MB

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Configuración corregida para Next.js 16 */
+  /* 1. Permitimos librerías externas rebeldes */
   serverExternalPackages: ['@prisma/client', 'langchain', 'pdf-parse', 'mammoth', 'xlsx'],
+  
+  /* 2. Aumentamos el límite de tamaño para archivos (Server Actions) */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '20mb', // Ahora puedes subir archivos de hasta 20 MB
+    },
+  },
 };
 
 export default nextConfig;
